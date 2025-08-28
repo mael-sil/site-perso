44023708b038f13e52a443790451b217f0e4d91e
mael-silvestre <silvestre-siaz.mael@protonmail.com>
2025-08-28 21:02:50 +0200

    Add base URL configuration for production and development environments in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === 'production' ? '/site-perso/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
