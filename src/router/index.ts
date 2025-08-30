3025efea551c0f955cf3485d8e509897d2231b29
mael-silvestre <silvestre-siaz.mael@protonmail.com>
2025-08-30 14:51:54 +0200

    Add scroll behavior to router for consistent page navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,11 @@ import { getRoutes } from '../utils/pages'
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: getRoutes(),
+  scrollBehavior() {
+    return new Promise((resolve) => {
+      resolve({ left: 0, top: 0 })
+    })
+  }
 })
 
 export default router
